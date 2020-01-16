66132adf2087019dd0bf4210f3bbe754556641f4
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-16 21:33:23 +0530

    Index page for Category entity
    Co-Authored-By: AppXive Technology <appxive@users.noreply.github.com>

---
+++ RazorCrudApp/RazorCrudApp/Pages/category/Index.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorCrudApp.AppDb;
+using RazorCrudApp.Models;
+
+namespace RazorCrudApp.Pages.category
+{
+    public class IndexModel : PageModel
+    {
+        private AppDbContext _context;
+
+        public IndexModel(AppDbContext context)
+        {
+            _context = context;
+        }
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public IEnumerable<Category> Categories { get; set; }
+
+        public void OnGet()
+        {
+            Categories = _context.Categories.ToList();
+        }
+    }
+}
