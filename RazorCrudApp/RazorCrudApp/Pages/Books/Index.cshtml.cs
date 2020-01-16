976e93893e3791058ac19b0d07f14b781b1496a4
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-16 18:07:21 +0530

    Update Index.cshtml
    Added Delete handler into index.cshtml
    
    Co-Authored-By: AppXive Technology <appxive@users.noreply.github.com>

--- RazorCrudApp/RazorCrudApp/Pages/Books/Index.cshtml.cs
+++ RazorCrudApp/RazorCrudApp/Pages/Books/Index.cshtml.cs
@@ -26,6 +26,21 @@ namespace RazorCrudApp.Pages.Books
             Books = _context.Books.ToList();
         }
 
-
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            try
+            {
+                var book = await _context.Books.FindAsync(id);
+                if (book == null) return BadRequest();
+                _context.Books.Remove(book);
+                var isDeleted = await _context.SaveChangesAsync();
+                if (isDeleted > 0) return RedirectToPage("Index");
+                return BadRequest();
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+        }
     }
 }
