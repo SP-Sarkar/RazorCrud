d29467e51b5d9c2fda7520db50b79a36f0c7e937
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-16 15:21:37 +0530

    Created Book Create page
    Added new Create page for book entity
    
    Co-Authored-By: AppXive Technology <appxive@users.noreply.github.com>

---
+++ RazorCrudApp/RazorCrudApp/Pages/Books/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorCrudApp.AppDb;
+using RazorCrudApp.Models;
+
+namespace RazorCrudApp.Pages.Books
+{
+    public class CreateModel : PageModel
+    {
+        private AppDbContext _context;
+
+        public CreateModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public void OnGet()
+        {
+            
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            try
+            {
+                if (!ModelState.IsValid) return Page();
+                _context.Books.Add(Book);
+                var isAdded = await _context.SaveChangesAsync().ConfigureAwait(true);
+                return isAdded > 0 ? RedirectToPage("Index") : RedirectToPage("./Error");
+            }
+            catch (Exception e)
+            {
+                return RedirectToPage("./Error");
+            }
+        }
+        
+    }
+}
