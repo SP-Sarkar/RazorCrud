fa46f4139015afda9e644b0728151a38070554f4
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-16 14:01:08 +0530

    Book Index Page
    New index page for Book and show all the existing book information of book table.
    
    Co-Authored-By: AppXive Technology <appxive@users.noreply.github.com>

--- RazorCrudApp/RazorCrudApp/Pages/Books/Index.cshtml.cs
+++ RazorCrudApp/RazorCrudApp/Pages/Books/Index.cshtml.cs
@@ -4,14 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorCrudApp.AppDb;
+using RazorCrudApp.Models;
 
 namespace RazorCrudApp.Pages.Books
 {
     public class IndexModel : PageModel
     {
+        private AppDbContext _context;
+
+        public IndexModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Book Book { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+        
         public void OnGet()
         {
-
+            Books = _context.Books.ToList();
         }
+
+
     }
 }
