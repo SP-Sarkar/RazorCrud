4e0772c4a7e7f65d8d4bbcaf2291a08fe93e4ee2
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-17 16:54:43 +0530

    AddAuthor Migration
    Co-Authored-By: AppXive Technology <appxive@users.noreply.github.com>

---
+++ RazorCrudApp/RazorCrudApp/Migrations/20200117111516_AddAuthor.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorCrudApp.Migrations
+{
+    public partial class AddAuthor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "Books",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 200, nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    DOB = table.Column<DateTime>(nullable: false),
+                    MobileNo = table.Column<string>(maxLength: 10, nullable: true),
+                    Website = table.Column<string>(maxLength: 100, nullable: true),
+                    Bio = table.Column<string>(maxLength: 2000, nullable: true),
+                    Image = table.Column<string>(maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Books");
+        }
+    }
+}
