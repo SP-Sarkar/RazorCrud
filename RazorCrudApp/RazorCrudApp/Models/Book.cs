31312055f6c2166a1adc8badd449f0e14ee900f9
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-17 13:52:55 +0530

    Added Display Attribute
    Added Display Attribute for user fiendly lable names
    
    Co-Authored-By: AppXive Technology <appxive@users.noreply.github.com>

--- RazorCrudApp/RazorCrudApp/Models/Book.cs
+++ RazorCrudApp/RazorCrudApp/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -12,12 +13,19 @@ namespace RazorCrudApp.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [Display(Name = "Book Name")]
         public string Name { get; set; }
+        
         [Required]
+        [Display(Name = "Book ISBN")]
         public string ISBN { get; set; }
+
+        [Display(Name = "Published Date")]
         public DateTime PublishedDate { get; set; }
 
         [ForeignKey("Category")]
+        [Required]
+        [Display(Name="Category Name")]
         public int CategoryId { get; set; }
         public Category Category { get; set; }
     }
--- RazorCrudApp/RazorCrudApp/Models/Category.cs
+++ RazorCrudApp/RazorCrudApp/Models/Category.cs
@@ -10,7 +10,9 @@ namespace RazorCrudApp.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
+        [Display(Name="Category Name")]
         public string Name { get; set; }
         public IEnumerable<Book> Books { get; set; }
     }
