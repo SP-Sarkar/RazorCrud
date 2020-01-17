b35c4b32ab8aa36d8e0bfbdbfc87ab073b7ab5db
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-17 10:30:40 +0530

    Drop Database and Updated Book And category
    Drop the old Database. And Added category into book for showing selectlist in book edit and create page. And also added Book into category to display all the books associated with specific category.

--- RazorCrudApp/RazorCrudApp/Models/Book.cs
+++ RazorCrudApp/RazorCrudApp/Models/Book.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +9,16 @@ namespace RazorCrudApp.Models
 {
     public class Book
     { 
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string ISBN { get; set; }
         public DateTime PublishedDate { get; set; }
 
+        [ForeignKey("Category")]
         public int CategoryId { get; set; }
-        public virtual Category Category { get; set; }
+        public Category Category { get; set; }
     }
 }
