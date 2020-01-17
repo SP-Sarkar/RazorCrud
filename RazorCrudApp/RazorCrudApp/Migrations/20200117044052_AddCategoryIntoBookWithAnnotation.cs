2f95473a9e79c97ea24a70d5f2a3b2d7992a026a
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-17 10:31:13 +0530

    Migration

---
+++ RazorCrudApp/RazorCrudApp/Migrations/20200117044052_AddCategoryIntoBookWithAnnotation.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorCrudApp.Migrations
+{
+    public partial class AddCategoryIntoBookWithAnnotation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Books",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ISBN",
+                table: "Books",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Books",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_CategoryId",
+                table: "Books",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Categories_CategoryId",
+                table: "Books",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Categories_CategoryId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_CategoryId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Books",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ISBN",
+                table: "Books",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
